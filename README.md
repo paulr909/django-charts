994e7702395247521d635b1fe2961a40cb8b993a
paulrogers <paul303808909@protonmail.com>
2022-01-14 17:30:25 +0000

    bump django to v4

--- README.md
+++ README.md
@@ -1,21 +1,18 @@
 # Django with Highcharts and Chart-JS
 
 [![Python Version](https://img.shields.io/badge/python-3.8-brightgreen.svg)](https://python.org)
-[![Django Version](https://img.shields.io/badge/django-3.2-brightgreen.svg)](https://djangoproject.com)
+[![Django Version](https://img.shields.io/badge/django-4.0.1-brightgreen.svg)](https://djangoproject.com)
 
 Install the requirements:
-
 ```bash
 pip install -r requirements.txt
-```
 
-```bash
 python manage.py runserver
 ```
 
-The project will be available at **127.0.0.1:8000**.
+The project will be available at [127.0.0.1:8000](127.0.0.1:8000)
 
-## API's 
+## APIs 
 [http://127.0.0.1:8000/api/v1/cities/](http://127.0.0.1:8000/api/v1/cities/)
 
 [http://127.0.0.1:8000/api/v1/browsers/](http://127.0.0.1:8000/api/v1/browsers/)
--- project/settings.py
+++ project/settings.py
@@ -1,7 +1,10 @@
 import os
 from decouple import config, Csv
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY")
 
@@ -39,7 +42,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            os.path.join(BASE_DIR, "templates"),
+            BASE_DIR / "templates",
         ],
         "APP_DIRS": True,
         "OPTIONS": {
@@ -58,7 +61,7 @@ WSGI_APPLICATION = "project.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
